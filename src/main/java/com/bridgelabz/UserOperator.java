63e7cc14fbd4abcf681947ee4eddc3acc34b693d
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-05-21 08:01:23 +0530

    Added: Last name valid or not

--- src/main/java/com/bridgelabz/UserOperator.java
+++ src/main/java/com/bridgelabz/UserOperator.java
@@ -1,23 +1,38 @@
 package com.bridgelabz;
-
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserOperator {
-    public void firstName() {
+    public void firstName(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first name ");
+        System.out.println("Enter the first name");
         String firstName = sc.nextLine();
         String regex = "^[A-Z][a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(firstName);
-        boolean valid = matcher.matches();
-        if (valid == true) {
-            System.out.println("Your name " + firstName + " is Valid");
-        } else {
-            System.out.println("Your name " + firstName + " is Invalid");
+        boolean valid=matcher.matches();
+        if (valid==true){
+            System.out.println("Your name "+firstName+" is Valid");
+        }
+        else{
+            System.out.println("Your name "+firstName+" is Invalid");
+        }
+    }
+
+    public void lastName(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the last name");
+        String lastName = sc.nextLine();
+        String regex = "^[A-Z][a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        boolean valid=matcher.matches();
+        if (valid==true){
+            System.out.println("Your name "+lastName+" is Valid");
+        }
+        else{
+            System.out.println("Your name "+lastName+" is Invalid");
         }
     }
 }
-
--- src/main/java/com/bridgelabz/UserRegistrationRegex.java
+++ src/main/java/com/bridgelabz/UserRegistrationRegex.java
@@ -1,9 +1,10 @@
 package com.bridgelabz;
 
 public class UserRegistrationRegex {
-    public static void main(String[]args) {
+    public static void main(String[] args) {
         System.out.println("Welcome to the user registration system.");
         UserOperator userOperator = new UserOperator();
         userOperator.firstName();
+        userOperator.lastName();
     }
 }
