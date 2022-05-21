9449683116c1d06007c5133f1d7b191bf5558606
suhantsakunde07 <sushantsakunde07@gmail.com>
2022-05-21 07:52:19 +0530

    Added master

---
+++ src/main/java/com/bridgelabz/UserOperator.java
@@ -0,0 +1,23 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserOperator {
+    public void firstName() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first name");
+        String firstName = sc.nextLine();
+        String regex = "^[A-Z][a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean valid = matcher.matches();
+        if (valid == true) {
+            System.out.println("Your name " + firstName + " is Valid");
+        } else {
+            System.out.println("Your name " + firstName + " is Invalid");
+        }
+    }
+}
+
---
+++ src/main/java/com/bridgelabz/UserRegistrationRegex.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class UserRegistrationRegex {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the user registration system.");
+        UserOperator userOperator = new UserOperator();
+        userOperator.firstName();
+    }
+}
